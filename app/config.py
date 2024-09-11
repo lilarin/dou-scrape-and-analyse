cbf2ec55265ff9e3f1146079be731162c172bf37
lilarin <80650911+lilarin@users.noreply.github.com>
2024-09-11 22:44:19 +0300

    Added separated config with technologies and domain

---
+++ app/config.py
@@ -0,0 +1,12 @@
+technologies = [
+    "Python", "Django", "Flask", "FastAPI", "SQL", "PostgreSQL", "MongoDB", "Docker",
+    "Kubernetes", "AWS", "Azure", "Linux", "Git", "JavaScript", "React", "Angular",
+    "Node.js", "TypeScript", "GraphQL", "Redis", "RabbitMQ", "Celery", "Jenkins",
+    "Terraform", "Nginx", "Apache", "Bootstrap", "SASS", "JQuery", "Vue.js", "PHP",
+    "Spring Boot", "Ruby on Rails", "Laravel", ".NET", "Java", "Kotlin", "Golang",
+    "Swift", "Rust", "Scala", "Kafka", "Grafana", "Sentry", "Datadog", "WordPress",
+    "JIRA", "Swagger", "Postman", "Figma", "GitLab", "Bitbucket", "C#", "Azure DevOps",
+     "Firebase", "Supabase", "Jupyter", "Apache Airflow", "OpenShift", "Sanity"
+]
+
+domain = "Python"

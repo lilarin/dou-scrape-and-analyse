8e4700707b34f84678ac77dc7c5adec099eccd34
lilarin <80650911+lilarin@users.noreply.github.com>
2024-09-11 15:42:42 +0300

    Added spider to crawl and scrape data

---
+++ scrape/config.py
@@ -0,0 +1,12 @@
+scrape_url = "https://jobs.dou.ua/vacancies/"
+
+scrape_position = "Python"
+
+save_dir = "data"
+
+experiences = {
+    "below year": "&exp=0-1",
+    "from 1 to 3 years": "&exp=1-3",
+    "from 3 to 5 years" : "&exp=3-5",
+    "greater that 5 years": "&exp=5plus",
+}
